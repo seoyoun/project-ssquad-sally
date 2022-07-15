afbd9a393894943b1dd4e96446cbed8586e9c623
Sally Lee <sallylee@wustl.edu>
2022-07-15 13:24:43 -0700

    redeploy-site.sh updated

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -6,10 +6,6 @@ cd /root/project-ssquad-sally
 #ensure git repo inside VPS has latest changes from main branch on GitHub
 git fetch && git reset origin/main --hard
 
-#enter python virtual env + install python dependencies
-python -m venv python3-virtualenv
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
+docker compose -f docker-compose.prod.yml down
 
-#restart myportfolio service
-systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml up -d --build
